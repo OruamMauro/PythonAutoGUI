9e05247de4e70edd5c5d97bd5d923095538a1d13
Mauro Delgadillo <mauro_delgadillo@outlook.com>
2020-07-07 11:46:26 -0500

    Add files via upload
    Enviar un saludo de manera automatizada por Skype utilizando pyautogui simulando la utilización de sólo el teclado.

---
+++ Skype_saludo.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.PAUSE = 2.5
+
+pyautogui.press('winleft')
+pyautogui.typewrite('skype\n')
+pyautogui.typewrite('DELGADILLO PINA Aldair')
+pyautogui.press('enter')
+pyautogui.typewrite('Test: Mensaje automático\n')
